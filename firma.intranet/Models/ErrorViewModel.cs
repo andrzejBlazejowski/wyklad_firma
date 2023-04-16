e83e582b531389341c3b0f51b5e0903197903a48
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-04-16 10:41:18 +0200

    Add project files.

---
+++ firma.intranet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace firma.intranet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
